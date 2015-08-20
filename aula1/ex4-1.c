e7564cd6547d5debe11b4717cf54ac81af089d7d
VICTOR MACEDO SABINO <b1312035@niger.grad.inf.puc-rio.br>
2015-08-20 13:07:30 -0300

    acabou de verdade

--- aula1/ex4-1.c
+++ aula1/ex4-1.c
@@ -3,10 +3,12 @@
 #include <sys/types.h>
 #include <stdlib.h>
 int main(){
+	char * args[3] = {"echo", "hello", NULL};
 	pid_t pid = fork();
 	if(pid == 0){
 		//processo filho
-		execve("ex4", NULL, 0);
+		//execve("ex4", NULL, 0);
+		execve("/bin/echo", args , 0);
 	}
 	else{
 		waitpid(-1, NULL, 0);
